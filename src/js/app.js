83d8372d6a9892245d490f6e1e0ca4255b3f5792
Alex Smith <alex.vesson.smith@gmail.com>
2020-12-06 21:15:04 +0300

    3d commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import game, {GameSavingData as GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from `./game`;
+import Game, {GameSavingData as GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from `./game`;
 
 const game = new Game();
 game.start();
